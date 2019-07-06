b7e8937dff9f87e5080a31edb19f410333dc1ad1
yusufcodes <yusufcodes@outlook.com>
2019-07-06 13:02:30 +0100

    Jokes now have an edit link This link will allow for the user to edit the text of the selected joke

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -22,12 +22,15 @@ echo htmlspecialchars($joke['email'], ENT_QUOTES,
         'UTF-8'); ?>"><?php
     echo htmlspecialchars($joke['name'], ENT_QUOTES,
         'UTF-8'); ?></a>)
+
+        <a href="editjoke.php?id=<?=$joke['id']?>">Edit</a>
         
         <form action="deletejoke.php" method="post">
             <input type="hidden" name="id" value="<?=$joke['id']?>">
             <input type="submit" value="Delete">
-
         </form>
+
+        
         
     </blockquote>
     
